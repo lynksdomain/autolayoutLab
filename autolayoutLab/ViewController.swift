91f91a3c50b3fbb2eedf4917b782ab2a8bf052c5
Alan Holguin <alanjholguin@gmail.com>
2018-11-14 14:25:13 -0500

    first commit

---
+++ autolayoutLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autolayoutLab
+//
+//  Created by C4Q on 11/14/18.
+//  Copyright © 2018 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("omg we are doing git!")
+    }
+
+
+}
+
